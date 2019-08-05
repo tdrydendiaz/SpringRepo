178a555d6ca6d43f41b1440e60b274af7b845026
tdrydendiaz <tdrydendiaz@gmail.com>
2019-08-05 17:26:50 +0100

    service shizzle

--- Users/src/main/java/com/bae/repository/AccountRepo.java
+++ Users/src/main/java/com/bae/repository/AccountRepo.java
@@ -1,10 +1,14 @@
 package com.bae.repository;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bae.entity.Account;
 
 public interface AccountRepo extends JpaRepository<Account, Long> {
+
+	
 
 	
 }
--- Users/src/main/java/com/bae/service/AccountServiceImpl.java
+++ Users/src/main/java/com/bae/service/AccountServiceImpl.java
@@ -9,38 +9,30 @@ import com.bae.repository.AccountRepo;
 
 public class AccountServiceImpl implements AccountService {
 
-	
 	@Autowired
 	private AccountRepo repo;
-	
-	
-	
+
+	@Override
 	public static Collection<Account> getAllAccounts() {
-	
-		return null;
+		return AccountRepo.getAllAccounts();
 	}
 
-
 	public static Collection<Account> createAccount() {
-	
 		return null;
+
 	}
 
 	public static Collection<Account> deleteAccount() {
-	
+
 		return null;
 	}
 
-	public static Collection<Account> getAnAccount() {
-	
-		return null;
+	public static Collection<Account> getAnAccount(int id) {
+		return repo.getAnAccount(id);
 	}
 
 	public static Collection<Account> updateAccount(int id, String accountNumber) {
-		
-		return null;
+
+		return repo.updateAccount(id, accountNumber);
 	}
 }
-
-
-	
